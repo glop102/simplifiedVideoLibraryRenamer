e7d4ecab1f1423dba0391a8ef09b11347d43ba21
glop102 <alexlikeschess@gmail.com>
2015-07-20 13:39:54 -0400

    Upgraded Settings Window - File Dialog for new library location

--- SettingsWindow.py
+++ SettingsWindow.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from std_os import files
 
 class SettingsWindow(Toplevel):
 	def __init__(self,parent):
@@ -9,6 +10,12 @@ class SettingsWindow(Toplevel):
 		Toplevel.grab_set(self)
 		Toplevel.protocol(self,"WM_DELETE_WINDOW",self.exit)
 
+	def browse(self,ignore=None):
+		temp=files()
+		newLoc=temp.askfolder(self.entryLocation.get())
+		if(len(newLoc)==0):return
+		self.entryLocation.delete(0,END)
+		self.entryLocation.insert(0,newLoc)
 	def confirm(self):
 		self.parent.location=self.entryLocation.get()
 		self.parent.populateShows() #restart the process from scratch
@@ -19,6 +26,8 @@ class SettingsWindow(Toplevel):
 	def body(self):
 		self.labelLocation=Label(self,text="Library Location: ")
 		self.entryLocation=Entry(self)
+		self.entryLocation.insert(0,self.parent.location)
+		self.entryLocation.bind("<Button-1>",self.browse) #use a file dialog to get the new location
 		self.labelLocation.grid(row=0,column=0)
 		self.entryLocation.grid(row=0,column=1)
 
